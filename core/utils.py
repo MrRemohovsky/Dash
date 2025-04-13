a1336533a352cee005ddad6c5dedea865fad2b77
AndreyR <arshanov8807@mail.ru>
2025-04-13 18:27:28 +0300

    Возможность добавлять и редактировать роли пользователям

--- core/utils.py
+++ core/utils.py
@@ -15,3 +15,18 @@ def login_required(view_func):
             return redirect(url_for('session.login'))
         return view_func(*args, **kwargs)
     return decorated_view
+
+def admin_required(view_func):
+    @wraps(view_func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.is_authenticated:
+            flash('Отказано в доступе!', 'danger')
+            return redirect(url_for('session.login'))
+
+        user_roles = getattr(current_user, 'roles', [])
+
+        if isinstance(user_roles, list) and any(getattr(role, 'name', '') == 'admin' for role in user_roles):
+            return view_func(*args, **kwargs)
+
+        return redirect('/charts')
+    return decorated_view
--- routes/admin.py
+++ routes/admin.py
@@ -1,27 +1,28 @@
 import uuid
-from flask import Blueprint, request, render_template, flash, redirect, url_for, jsonify, get_flashed_messages
+from flask import Blueprint, request, render_template, flash, jsonify, get_flashed_messages
 from flask_security import hash_password, current_user
-from core.utils import login_required
-from models import User, db
+from core.utils import admin_required
+from models import User, db, Role
 
 admin = Blueprint('admin', __name__)
 
 
 @admin.route('/admin_panel')
-@login_required
+@admin_required
 def get_all_users():
     page = request.args.get('page', 1, type=int)
     pagination = User.query.filter_by(active=True).paginate(page=page, per_page=10)
     return render_template('admin_panel.html', pagination=pagination)
 
 @admin.route('/admin_panel/create', methods=['POST'])
-@login_required
+@admin_required
 def create_user():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     patronym = request.form['patronym']
     email = request.form['email']
     password = request.form['password']
+    role_ids = request.form.getlist('roles')
 
     if User.query.filter_by(email=email).first():
         flash('Пользователь с таким email уже существует.', 'warning')
@@ -40,6 +41,12 @@ def create_user():
             password=hash_password(password),
             active=True,
         )
+        if role_ids:
+            roles = Role.query.filter(Role.id.in_(role_ids)).all()
+            new_user.roles = roles
+        else:
+            new_user.roles = []
+
         db.session.add(new_user)
         db.session.commit()
         flash('Пользователь успешно создан.', 'success')
@@ -56,7 +63,7 @@ def create_user():
 
 
 @admin.route('/admin_panel/deactivate/<string:user_id>', methods=['POST'])
-@login_required
+@admin_required
 def deactivate_user(user_id):
     user = User.query.filter_by(id=user_id).first()
 
@@ -71,7 +78,7 @@ def deactivate_user(user_id):
     return jsonify(success=True)
 
 @admin.route('/admin_panel/edit', methods=['POST'])
-@login_required
+@admin_required
 def edit_user():
     user_id = request.form.get('user_id')
     first_name = request.form.get('first_name')
@@ -79,6 +86,7 @@ def edit_user():
     patronym = request.form.get('patronym')
     email = request.form.get('email')
     password = request.form.get('password')
+    role_ids = request.form.getlist('roles')
 
     if not all([user_id, first_name, last_name, patronym, email]):
         flash('Заполните все поля.', 'warning')
@@ -97,6 +105,12 @@ def edit_user():
         flash('Вы не можете редактировать данного пользователя!', 'danger')
         return jsonify({'success': False, 'messages': get_flashed_messages(with_categories=True)})
 
+    if role_ids:
+        roles = Role.query.filter(Role.id.in_(role_ids)).all()
+        user.roles = roles
+    else:
+        user.roles = []
+
     user.first_name = first_name
     user.last_name = last_name
     user.patronym = patronym
@@ -108,7 +122,7 @@ def edit_user():
     return jsonify({'success': True, 'messages': get_flashed_messages(with_categories=True)})
 
 @admin.route('/admin/get_user/<string:user_id>')
-@login_required
+@admin_required
 def get_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     if user:
@@ -117,7 +131,12 @@ def get_user(user_id):
             "first_name": user.first_name,
             "last_name": user.last_name,
             "patronym": user.patronym,
-            "email": user.email
+            "email": user.email,
+            'roles': [{'id': role.id, 'name': role.name, 'description': role.description} for role in user.roles]
         })
-    else:
-        return jsonify({})
+
+@admin.route('/get_roles')
+@admin_required
+def get_roles():
+    roles = Role.query.all()
+    return jsonify([{'id': r.id, 'name': r.name} for r in roles])
